b336590670ec7050d5a436ffd718f73383fef2f8
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-05-25 15:05:39 +0600

    Appointment Route Added

--- src/Routes/Routers/Router.js
+++ src/Routes/Routers/Router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
+import Appointment from "../../pages/Appointment/Appointment/Appointment";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/appointment",
+        element: <Appointment />,
       },
       {
         path: "/login",
---
+++ src/pages/Appointment/Appointment/Appointment.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AppointmentBanner from "../AppointmentBanner/AppointmentBanner";
+import Footer from "../../../Shared/Footer/Footer";
+
+const Appointment = () => {
+  return (
+    <div className="px-6">
+      <AppointmentBanner />
+      <Footer />
+    </div>
+  );
+};
+
+export default Appointment;
